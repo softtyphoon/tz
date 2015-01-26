deb1c6c0b3a8c5f02b16924acc2a0b9d902d337f
unknown <03575@W-03575-01.accelink.com>
2015-01-26 08:25:33 +0800

    add qiaqia

---
+++ tools/qiaqiafood.py
@@ -0,0 +1,85 @@
+
+
+import argparse
+import urllib2
+import urllib
+import os
+import re
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
+import httplib
+import StringIO, gzip
+import random
+import time
+
+_file_name = 'qiaqiafood.txt'
+_url = 'http://hongbao.qiaqiafood.com/action.php'
+# _file = None
+_header = {'Origin': 'http://hongbao.qiaqiafood.com',
+           'Accept': '*/*',
+           'X-Requested-With': 'XMLHttpRequest',
+           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+           'Accept-Language': 'zh-CN,zh;q=0.8',
+           'Referer': 'http://hongbao.qiaqiafood.com/',
+           'Accept-Encoding': 'gzip, deflate',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.48 Safari/537.36 QQBrowser/8.0.3197.400',
+           'Host': 'hongbao.qiaqiafood.com',
+           'Content-Length': '34',
+           'Proxy-Connection': 'Keep-Alive',
+           'Pragma': 'no-cache',
+           'Cookie': 'PHPSESSID=3j98rbnq804eaas6jel0qcpqn2; CNZZDATA1253517292=528758234-1422188634-%7C1422188634'}
+
+def gzdecode(data):
+  compressedstream = StringIO.StringIO(data)
+  gziper = gzip.GzipFile(fileobj=compressedstream)
+  data2 = gziper.read()
+  return data2
+
+def try_code(num):
+  opener = urllib2.OpenerDirector()
+  handler = urllib2.HTTPHandler()
+  opener.add_handler(handler)
+  req = urllib2.Request(_url)
+  data = 'action=pcticket&SNkey='+num
+  for (name, val) in _header.items():
+    req.add_header(name, val)
+  req.add_data(data)
+  try:
+    response = opener.open(req)
+  except:
+    # print '10060 error, sleep 10 second'
+    print 'sleep'
+    time.sleep(10)
+    return '0'
+  content = response.read()
+  bs_page =  BeautifulSoup(gzdecode(content))
+  # print bs_page.contents[0]
+  return bs_page.contents[0][11]
+
+def code_write(fp, num):
+  fp.writelines(num)
+
+def main():
+  _file = open(_file_name, 'a')
+  while True:
+    code = ''
+    for i in range(1, 13):
+      a = random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+      code = code + a
+    print 'try ->>>>>>>>>>>>>>>>>>>>>>>'+code
+    d = try_code(code)
+    # d = '0'
+    # print code+'-------------------->'+d
+    if d == '0':
+      continue
+      break
+    else:
+      code_write(_file, code)
+      break
+  _file.flush()
+  _file.close()
+
+
+if __name__ == '__main__':
+  main()
