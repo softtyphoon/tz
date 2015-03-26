06b2caf301f31bef2d7ef37c8bafc352068f5107
unknown <03575@W-03575-01.accelink.com>
2015-03-26 16:25:27 +0800

    fdf

--- tools/p2p/firstp2p.py
+++ tools/p2p/firstp2p.py
@@ -1,6 +1,7 @@
 ﻿#codng:utf-8
 from bs4 import BeautifulSoup
-
+import sys
+import urllib2
 
 type = {
     'crd':u'产融贷',         # icon_melting
@@ -9,30 +10,93 @@ type = {
 }
 
 
-def get_primary_table():
-    return 0
 
+
+
+
+main_url = u'http://www.firstp2p.com/'
+_header = {
+    u'Host':u'www.firstp2p.com',
+    u'User-Agent':u'Mozilla/5.0 (Windows NT 5.1; rv:36.0) Gecko/20100101 Firefox/36.0',
+    u'Accept':u'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    u'Accept-Language':u'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+    u'Accept-Encoding':u'gzip, deflate',
+    u'Referer':u'http://www.firstp2p.com/',
+    u'Cookie':u'PHPSESSID=emk4jof8co8osccrrfao3vu0b0; _ncfdm=; _ncftr=; _ncf1=1427351222717.6764.0003.0002.0003.0002.0; _ncf2=1427351222717.8047.0003.0002; _ncf3=1427351254278.3.31561.6574.1.61744; mlta=%7B%22mltn%22%3A%7B%22250582%22%3A%5B%226729839450975941527%3E2%3E1427351254237%3E1%3E1427351254180%3E6729839450949563443%3E1427351228421%22%2C1442903254393%5D%7D%2C%22mlti%22%3A%7B%22250582%22%3A%5B%22142735122633671481%22%2C1442903226336%5D%7D%2C%22mltmapping%22%3A%7B%220%22%3A%5B1%2C1429943254395%5D%7D%2C%22mlts%22%3A%7B%22250582%22%3A%5B%225%22%2C1442903254334%5D%7D%7D; fpid=449038',
+    u'Connection':u'keep-alive'
+}
+
+
+def get_primary_table(url = main_url, header = _header, data = None):
+    opener = urllib2.OpenerDirector()
+    http_handler = urllib2.HTTPHandler()
+    https_handler = urllib2.HTTPSHandler()
+    opener.add_handler(http_handler)
+    opener.add_handler(https_handler)
+    req = urllib2.Request(url)
+    for (name, val) in header.items():
+        req.add_header(name, val)
+    if data is not None:
+        req.add_data(self.data)
+
+    try:
+        r = opener.open(req, timeout = 60)
+    except:
+        print 'failed'
+        opener.close()
+        r.close()
+        sys.exit(0)
+    # Make sure everything is working ;)
+    if r.info().get('Content-Encoding') == 'gzip':
+        buf = StringIO.StringIO(r.read())
+        f = gzip.GzipFile(fileobj=buf)
+        data = f.read()
+    else:
+        data = r.read()
+
+    # extract valid URLs
+    fund_url = list()
+    bs = BeautifulSoup(data)
+    fund_div = bs.find_all('div', attrs={'id':'index_list_tab'})
+    fund_div_s = fund_div[0].find_all('div', attrs={'class':'pro_name'})
+    for a in fund_div_s:
+      atag =  a.find('a')
+      if atag is None:
+          continue
+      fund_url.append(atag['href'])
+    return fund_url
 
 
 def get_sub_table():
     return 0
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
+    fund_list = get_primary_table()
+    print repr(fund_list)
+    sys.exit(0)
     fn = open('a.txt', 'r+')
     c = fn.read()
     bs = BeautifulSoup(c)
+    fund_url = list()
+    fund_div = bs.find_all('div', attrs={'id':'index_list_tab'})
+    fund_div_s = fund_div[0].find_all('div', attrs={'class':'pro_name'})
+    for a in fund_div_s:
+      atag =  a.find('a')
+      if atag is None:
+          continue
+      fund_url.append(atag['href'])
+    print fund_url
+    sys.exit(0)
     b = bs.find_all('div', attrs={'id':'index_list_tab'})
-    d = b[0].find_all('div', attrs={'class':'tc'})
+    # d = b[0].find_all('div', attrs={'class':'tc'})
+    d = b[0].find_all('div', attrs={'class':'pro_name'})
     for a in d:
-      print a.contents[0]
-    # print repr(d)
+      # print a.contents[0].contents[0]
+      atag =  a.find('a')
+      if atag is None:
+          continue
+      print atag['href']
+    print '+'
 
 
 
---
+++ tools/vote_crawler/new  1.v
@@ -0,0 +1,39 @@
+
+14:28:36.112[570ms][total 8530ms] Status: 200[OK]
+GET http://www.firstp2p.com/account Load Flags[LOAD_DOCUMENT_URI  LOAD_INITIAL_DOCUMENT_URI  ] Content Size[-1] Mime Type[text/html]
+   Request Headers:
+      Host[www.firstp2p.com]
+      User-Agent[Mozilla/5.0 (Windows NT 5.1; rv:36.0) Gecko/20100101 Firefox/36.0]
+      Accept[text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8]
+      Accept-Language[zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3]
+      Accept-Encoding[gzip, deflate]
+      Referer[http://www.firstp2p.com/]
+      Cookie[PHPSESSID=emk4jof8co8osccrrfao3vu0b0; _ncfdm=; _ncftr=; _ncf1=1427351222717.6764.0003.0002.0003.0002.0; _ncf2=1427351222717.8047.0003.0002; _ncf3=1427351254278.3.31561.6574.1.61744; mlta=%7B%22mltn%22%3A%7B%22250582%22%3A%5B%226729839450975941527%3E2%3E1427351254237%3E1%3E1427351254180%3E6729839450949563443%3E1427351228421%22%2C1442903254393%5D%7D%2C%22mlti%22%3A%7B%22250582%22%3A%5B%22142735122633671481%22%2C1442903226336%5D%7D%2C%22mltmapping%22%3A%7B%220%22%3A%5B1%2C1429943254395%5D%7D%2C%22mlts%22%3A%7B%22250582%22%3A%5B%225%22%2C1442903254334%5D%7D%7D; fpid=449038]
+      Connection[keep-alive]
+   Response Headers:
+      Date[Thu, 26 Mar 2015 06:28:36 GMT]
+      Server[nginx/1.2.4]
+      Content-Type[text/html; charset=utf-8]
+      Vary[Accept-Encoding]
+      X-Powered-By[PHP/5.4.36]
+      Expires[Thu, 19 Nov 1981 08:52:00 GMT]
+      Pragma[no-cache]
+      Cache-Control[private]
+      server_number[9]
+      Via[1.1 ID-0000627501376404 uproxy-3]
+      Set-Cookie[fpid=449038; path=/]
+      Transfer-Encoding[chunked]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
